e8158b566cf550e2c7dfb05be432cf18e4aa0b17
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-07-23 15:57:42 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { lazy, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
@@ -6,11 +6,15 @@ import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks/useAuth';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
+import Home from 'pages/Home';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
+import Contacts from 'pages/Contacts';
 
-const HomePage = lazy(() => import('../pages/Home'));
-const RegisterPage = lazy(() => import('../pages/Register'));
-const LoginPage = lazy(() => import('../pages/Login'));
-const ContactsPage = lazy(() => import('../pages/Contacts'));
+// const HomePage = lazy(() => import('../pages/Home'));
+// const RegisterPage = lazy(() => import('../pages/Register'));
+// const LoginPage = lazy(() => import('../pages/Login'));
+// const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -24,24 +28,20 @@ export const App = () => {
   ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
+        <Route index element={<Home />} />
         <Route
           path="/register"
           element={
-            <RestrictedRoute component={RegisterPage} redirectTo="/contacts" />
+            <RestrictedRoute component={Register} redirectTo="/contacts" />
           }
         />
         <Route
           path="/login"
-          element={
-            <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
-          }
+          element={<RestrictedRoute component={Login} redirectTo="/contacts" />}
         />
         <Route
           path="/contacts"
-          element={
-            <PrivateRoute component={ContactsPage} redirectTo="/login" />
-          }
+          element={<PrivateRoute component={Contacts} redirectTo="/login" />}
         />
         <Route path="*" element={<h2>Not found</h2>} />
       </Route>
